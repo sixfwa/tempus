6d8f3813b4a58167b2afecf4627920ec0fa24acd
sixfwa <farhanaliw6@gmail.com>
2020-07-13 22:14:10 +0100

    select worksheet, security added to today method

--- app/sheets_commands.py
+++ app/sheets_commands.py
@@ -1,3 +1,4 @@
+from gspread.exceptions import WorksheetNotFound, CellNotFound
 from connect import client
 from datetime import date
 
@@ -8,16 +9,32 @@ class Secretary:
     def __init__(self):
         # Open the Tempus spreadsheet
         self.spreadsheet = client.open("tempus")
+        self.worksheet = None
+        self.today_created = False
 
-    def create_work_sheet(self, name, rows, cols):
+    def create_work_sheet(self, name, rows="1000", cols="27"):
         # create a worksheet
         self.spreadsheet.add_worksheet(title=name, rows=rows, cols=cols)
 
+    def select_work_sheet(self, name):
+        # select worksheet if it exists
+        try:
+            self.worksheet = self.spreadsheet.worksheet(name)
+        except WorksheetNotFound:
+            print("Worksheet does not exist")
+
+    def add_activity(self, activity_name):
+        self.worksheet.append_column(activity_name)
+
     def today(self):
-        # create the row for today
-        today = [date.today().strftime('%Y-%m-%d'), ]
-        self.spreadsheet.append_row(today, 'USER_ENTERED')
+        # create the row for today if it doesn't exist
+        try:
+            today = [date.today().strftime('%Y-%m-%d'), ]
+            cell = self.worksheet.find(today[0])
+        except CellNotFound:
+            self.worksheet.append_row(today, 'USER_ENTERED')
 
 
 secretary = Secretary()
-secretary.create_work_sheet("test", rows="1000", cols="27")
+secretary.select_work_sheet("test")
+secretary.add_activity("Date")
