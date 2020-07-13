8b5b5990223168c864b6d2a2f9aca270184819aa
sixfwa <farhanaliw6@gmail.com>
2020-07-14 00:34:12 +0100

    work sheet commands and clear terminal

--- app/commands.py
+++ app/commands.py
@@ -1,9 +1,15 @@
 SHOW_WORKSHEETS = "show worksheets"
 SHOW_COMMANDS = "show commands"
+CLEAR_SCREEN = "clear screen"
+SELECT_WORKSHEET = "select worksheet"
+SHOW_SELECTED_WORKSHEET = "show selected worksheet"
 BYE = "bye"
 
 commands = [
     SHOW_WORKSHEETS,
     SHOW_COMMANDS,
+    SELECT_WORKSHEET,
+    CLEAR_SCREEN,
+    SHOW_SELECTED_WORKSHEET,
     BYE
 ]
