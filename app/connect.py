126e0e0e38550b1a50e416e31de79ea5a6b8dd01
sixfwa <farhanaliw6@gmail.com>
2020-07-13 20:00:14 +0100

    connect to google sheets

---
+++ app/connect.py
@@ -0,0 +1,11 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    "credentials.json", scope)
+
+client = gspread.authorize(credentials)
