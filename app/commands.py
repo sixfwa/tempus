fd8de462910483bcfad560e361befddb9ad9c15b
sixfwa <farhanaliw6@gmail.com>
2020-07-14 01:26:35 +0100

    add activity and create worksheet

--- app/commands.py
+++ app/commands.py
@@ -2,14 +2,18 @@ SHOW_WORKSHEETS = "show worksheets"
 SHOW_COMMANDS = "show commands"
 CLEAR_SCREEN = "clear screen"
 SELECT_WORKSHEET = "select worksheet"
+CREATE_WORKSHEET = "create worksheet"
 SHOW_SELECTED_WORKSHEET = "show selected worksheet"
+ADD_ACTIVITY = "add activity"
 BYE = "bye"
 
 commands = [
     SHOW_WORKSHEETS,
     SHOW_COMMANDS,
-    SELECT_WORKSHEET,
-    CLEAR_SCREEN,
     SHOW_SELECTED_WORKSHEET,
+    CREATE_WORKSHEET,
+    SELECT_WORKSHEET,
+    ADD_ACTIVITY,
+    CLEAR_SCREEN,
     BYE
 ]
