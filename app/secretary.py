2fbf4e17ace2e08587b79a1405cd9e9d15c479c8
sixfwa <farhanaliw6@gmail.com>
2020-07-14 01:26:12 +0100

    initial setup

--- app/secretary.py
+++ app/secretary.py
@@ -14,8 +14,9 @@ class Secretary:
         """
         self.spreadsheet = client.open("tempus")
         self.worksheet = None
+        self.worksheet_name = ""
 
-    def create_work_sheet(self, name, rows="1000", cols="27"):
+    def create_worksheet(self, name, rows="1000", cols="27"):
         """
         Create the worksheet given the name and the number  of rows and columns.
         """
@@ -24,14 +25,19 @@ class Secretary:
     def get_worksheets(self):
         return self.spreadsheet.worksheets()
 
-    def select_work_sheet(self, name):
+    def select_worksheet(self, name):
         """
         Select the worksheet if it exists
         """
         try:
             self.worksheet = self.spreadsheet.worksheet(name)
+            self.worksheet_name = name
+            display.print_okgreen(f"\nWorksheet {name} selected\n")
         except WorksheetNotFound:
-            display.print_warning("Worksheet does not exist")
+            display.print_fail("\nWorksheet does not exist\n")
+
+    def worksheet_initial_setup(self):
+        self.worksheet.update_cell(1, 1, "DATE")
 
     def get_activities(self):
         return self.worksheet.row_values(1)
@@ -41,13 +47,13 @@ class Secretary:
         Add activity to the spreadsheet.
         """
         if activity_name.lower() in map(lambda x: x.lower(), self.get_activities()):
-            display.print_warning("Activity already exists.")
+            display.print_fail("\nActivity already exists.")
         else:
             # Get the number of activities
             number_of_activities = len(self.get_activities())
             # Append to the row of activities
             self.worksheet.update_cell(
-                1, number_of_activities + 1, activity_name)
+                1, number_of_activities + 1, activity_name.upper())
 
     def today(self):
         # create the row for today if it doesn't exist
